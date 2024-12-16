fa99382d3e6e93e358ca67dae451db41044220c9
pane111 <jakob.njh@gmail.com>
2024-12-16 19:37:17 +0100

    Basic enemy behaviour

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -10,15 +10,42 @@ public class Enemy : MonoBehaviour
     public float detectRange;
     public bool playerFound;
     public Rigidbody2D rb;
+
+    public Transform player;
     void Start()
     {
-        
+        player = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 pDir = player.position - transform.position;
+        pDir.Normalize();
+        RaycastHit2D hit;
+        hit = Physics2D.Raycast(transform.position, pDir, detectRange);
+        if (hit)
+        {
+            if (hit.transform == player)
+            {
+                playerFound = true;
+                GetComponent<Animator>().enabled = true;
+            }
+            else
+            {
+                playerFound = false;
+                GetComponent<Animator>().enabled = false;
+            }
+        }
+
+        if (playerFound)
+        {
+            rb.velocity = pDir * speed;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
     }
 
     public void TakeDamage(float amount)
